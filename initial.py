f7129ebda8edf07b6a4560c2f105effa21e9d507
Jamal Ahmed <jamal_ua@hotmail.com>
2020-02-01 00:43:43 +0000

    small changes

--- initial.py
+++ initial.py
@@ -1,3 +1,17 @@
 print('Bismillah')
-name = input('Who are you ')
-print('Hello', name )
+
+try:
+    a = input("Write the score => ")
+    if float(a) > 1.0:
+        print('Bad score')
+    elif float(a) >= 0.9:
+        print('A')
+    elif float(a) >= 0.8:
+        print('B')
+    elif float(a) >= 0.7:
+        print('C')
+    elif float(a) < 0.6:
+        print('F')
+except:
+    print("Bad score")
+
