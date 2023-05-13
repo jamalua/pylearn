1f14b4f0a1fb83acaa8050e749af55b5f6a65804
Jamal Ahmed <jamal_ua@hotmail.com>
2023-05-14 00:03:00 +0100

    added things to gitignore

--- classdemo.py
+++ classdemo.py
@@ -1,22 +1,26 @@
 class Staff:
-    def __init__(self,pPosition,pName,pPay):
+    def __init__(self, pPosition, pName, pPay):
         self.position = pPosition
         self.name = pName
         self.pay = pPay
         print("CreatedObject")
 
     def __str__(self):
-        return "Position = %s, Name = %s, Pay = %d" %(self.position, self.name, self.pay)
+        return "Position = %s, Name = %s, Pay = %d" % (
+            self.position,
+            self.name,
+            self.pay,
+        )
 
     def calculatePay(self):
-        prompt = "\nEnter number of hours worked for %s " %(self.name)
+        prompt = "\nEnter number of hours worked for %s " % (self.name)
         hours = input(prompt)
-        prompt = 'Enter the hourly rate for %s ' %(self.name)
+        prompt = "Enter the hourly rate for %s " % (self.name)
         hourlyRate = input(prompt)
-        self.pay = int(hours)*int(hourlyRate)
+        self.pay = int(hours) * int(hourlyRate)
         return self.pay
 
 
-officeStaff1 = Staff('Basic','Jamal', 10)
+officeStaff1 = Staff("Basic", "Jamal", 10)
 print(officeStaff1.calculatePay())
 print(officeStaff1)
