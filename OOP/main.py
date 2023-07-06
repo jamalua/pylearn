6cda7add4f058eed3b76549dd23b84701f84dc70
Jamal Ahmed <jamal_ua@hotmail.com>
2023-07-06 23:31:25 +0100

    commit

--- OOP/main.py
+++ OOP/main.py
@@ -1,9 +1,11 @@
 class Item:
-    
-    """ Description	"""
-    def calculate_total_price(self, x, y):
+    """Description"""
 
-        """ Description
+    pay_rate = 0.8  # The pay rate after 20% discount
+    all = list()
+
+    def __init__(self, name: str, price: float, quantity=0) -> None:
+        """Description
         :type self:
         :param self:
 
@@ -11,17 +13,45 @@ class Item:
 
         :rtype:
         """
-        return x * y
+        assert price >= 0, f"Price {price} is not greater than 0"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than 0"
+
+        print(f"An instance created for {name}")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    def calculate_total_price(self):
+        """Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        """Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
+        self.price = self.price * self.pay_rate
+
+    def __repr__(self) -> str:
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-item2 = Item()
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
-print(item2.calculate_total_price(item2.price, item2.quantity))
+print(Item.all)
--- test_pytestTutorial.py
+++ test_pytestTutorial.py
@@ -1,24 +1,20 @@
 from pytestTutorial import calculator
 
+
 def test_add():
     calc = calculator()
-    result = calc.add(2,3)
-    assert result == 5    
+    result = calc.add(2, 3)
+    assert result == 5
+
 
 def test_add_string():
     calc = calculator()
-    result = calc.add('Hello',' World') 
-    assert result == 'Hello World'
+    result = calc.add("Hello", " World")
+    assert result == "Hello World"
     assert type(result) is str
-    assert 'Hello' in result
+    assert "Hello" in result
+
 
 def test_product():
     calc = calculator()
-    assert calc.multiply(5,5) == 25
- 
- 
-
-
-
-
-
+    assert calc.multiply(5, 5) == 25
